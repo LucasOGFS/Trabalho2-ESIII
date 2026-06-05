aa1d359e4ae0df0c521a14141122227d3552f5d6
andrew-pravoslavie <gabriel.andrewbg@gmail.com>
2026-06-04 22:18:34 -0300

    Pequena atualização na entidade Ranking

--- src/entities/Ranking.java
+++ src/entities/Ranking.java
@@ -1,8 +1,29 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Ranking {
+    private static final int tamanho = 10;
     private List<RegistroRanking> ranking = new ArrayList<>();
+
+    public Ranking(List<RegistroRanking> registrosIniciais) {
+        if (registrosIniciais != null) {
+            this.ranking.addAll(registrosIniciais);
+            ordenarRanking();
+        }
+    }
+
+    private void ordenarRanking() {
+        ranking.sort(Comparator.comparingInt(RegistroRanking::getPontuacao).reversed());
+    }
+
+    public boolean isElegivel(int pontuacao) {
+        if (ranking.size() < tamanho) {
+            return true;
+        }
+        int menorPontuacaoAtual = ranking.get(ranking.size() - 1).getPontuacao();
+        return pontuacao > menorPontuacaoAtual;
+    }
 }
